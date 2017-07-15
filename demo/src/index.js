be10d51706aa60137acf25016bffb2e14db71ccb
Wickramaranga Abeygunawardhana <umstek@live.com>
2017-07-15 16:47:56 +0530

    Grid rendering logic

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,22 @@
-import React, {Component} from 'react'
-import {render} from 'react-dom'
-
-import Example from '../../src'
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+import Heatmap from '../../src';
 
 class Demo extends Component {
-  render() {
-    return <div>
-      <h1>react-grid-heatmap Demo</h1>
-      <Example/>
-    </div>
-  }
+    render() {
+        return (
+            <div>
+                <h1>react-grid-heatmap Demo</h1>
+                <Heatmap
+                    Dot={(props) => <rect {...props}/>}
+                    height={70 + 7} width={520 + 52}
+                    gutterSize={1}
+                    size={{cols: 52, rows: 7}}
+                    //hLabels={[]} vLabels={[]}
+                    grid={[1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0]}/>
+            </div>
+        );
+    }
 }
 
-render(<Demo/>, document.querySelector('#demo'))
+render(<Demo/>, document.querySelector('#demo'));
--- src/index.js
+++ src/index.js
@@ -1,9 +1,42 @@
 import React, {Component} from 'react'
 
-export default class extends Component {
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
-  }
+function dotSize(remainingSpace, dotCount, gutterSize) {
+    // _$_._._._._
+    return (remainingSpace - (dotCount) * gutterSize) / dotCount;
 }
+
+class Heatmap extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const {Dot, hLabels, vLabels, gutterSize, size, height, width} = this.props;
+
+        const dotHeight = dotSize(height, size.rows + !!hLabels, gutterSize);
+        let startY = gutterSize;
+        const dotWidth = dotSize(width, size.cols + !!vLabels, gutterSize);
+        let startX = gutterSize;
+
+        if (hLabels) {
+            // add horizontal labels
+            startY += gutterSize + dotHeight;
+        }
+        if (vLabels) {
+            // add vertical labels
+            startX += gutterSize + dotWidth;
+        }
+
+        return <svg height={height} width={width}>
+            {
+                this.props.grid.map((x, i, arr) =>
+                    <Dot key={i} item={x} items={arr}
+                         height={dotHeight} width={dotWidth}
+                         x={startX + (Math.floor(i / size.rows) * (dotWidth + gutterSize))}
+                         y={startY + (i % size.rows) * (dotHeight + gutterSize)}/>)
+            }
+        </svg>
+    }
+}
+
+export default Heatmap;
